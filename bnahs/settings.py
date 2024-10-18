d33f3ded027026360fa57abf61b2612282dcd534
MakieTech <121760681+astigPree@users.noreply.github.com>
2024-10-18 22:03:05 +0800

    put media directory

--- bnahs/settings.py
+++ bnahs/settings.py
@@ -130,6 +130,8 @@ USE_I18N = True
 STATIC_URL = 'static/' 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
