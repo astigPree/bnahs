429f3c7bcdaed075febae6b8545035c71f485bb1
MakieTech <121760681+astigPree@users.noreply.github.com>
2024-12-18 15:36:37 +0800

    fix it

--- backend/views_for_evaluator_2.py
+++ backend/views_for_evaluator_2.py
@@ -586,6 +586,8 @@ def evaluator_get_rpms_folder(request):
                 attachement = models.RPMSAttachment.objects.filter(employee_id=teacher_id, class_work_id=classwork.class_work_id).order_by('-created_at').first()
                 if attachement:
                     rpms_classworks.append(classwork.get_rpms_classwork_information( attachement ))
+                else:
+                    rpms_classworks.append(classwork.get_rpms_classwork_information())
             
             return JsonResponse({
                 'rpms_folder' : rpms_folder.get_rpms_folder_information(),
